b1c71c5774f046809b2acc02c01b1caa4b7556ba
MattRubbelke <matt.rubbelke@gmail.com>
2019-07-07 12:29:31 -0500

    heroku.

---
+++ scripts/seedDB.js
@@ -0,0 +1,35 @@
+    
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This script adds seed data for testing
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/patentapps"
+);
+
+const applicationSeed = [
+  {
+    fileNo: "01005.004US1",
+    appNo: "12352345",
+    PTA: 8
+  },
+  {
+    fileNo: "01111.004US1",
+    appNo: "13422345",
+    PTA: 459
+  }
+];
+
+db.App
+  .remove({})
+  .then(() => db.App.collection.insertMany(applicationSeed))
+  .then(data => {
+    console.log(data.result.n + " applications inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
